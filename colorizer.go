2052b172ecdb0f364ea200e4d5568bb85949758d
Omer <43470074+KaplanOmr@users.noreply.github.com>
2022-11-12 22:30:06 +0300

    Improvement to project struct

--- colorizer.go
+++ colorizer.go
@@ -2,6 +2,8 @@ package colorizer
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Colorizer struct {
@@ -21,17 +23,33 @@ const (
 )
 
 const (
-	_ANSI = "\033[%dm"
+	_ANSI = "\033[{ARGS}m"
 	RESET = 0
 )
 
-func (c *Colorizer) New(text string, color int) string {
-	ansiiColor := c.generateANSII(color)
-	ansiiReset := c.generateANSII(RESET)
+func New(text string, color int) string {
+	var c Colorizer
 
-	return ansiiColor + text + ansiiReset
+	return c.Make(text, color)
 }
 
-func (c *Colorizer) generateANSII(arg int) string {
-	return fmt.Sprintf(_ANSI, arg)
+func (c *Colorizer) Make(text string, color int) string {
+	c.generateTemplate(color)
+	c.generateText(text)
+
+	return c.Text
+}
+
+func (c *Colorizer) generateANSI(args string) string {
+	return strings.Replace(_ANSI, "{ARGS}", args, 1)
+}
+
+func (c *Colorizer) generateTemplate(color int) {
+	colorANSI := c.generateANSI(strconv.Itoa(color))
+	resetANSI := c.generateANSI(strconv.Itoa(RESET))
+	c.Template = fmt.Sprintf("%s{TEXT}%s", colorANSI, resetANSI)
+}
+
+func (c *Colorizer) generateText(text string) {
+	c.Text = strings.Replace(c.Template, "{TEXT}", text, 1)
 }
